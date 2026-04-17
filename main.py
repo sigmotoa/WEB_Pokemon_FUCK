e6cf9d027ca2b92f2fe18e92f47a2a19e68adf87
sigmotoa <si.galvismotoa@gmail.com>
2026-04-17 07:43:08 -0500

    [UPDATE] Find pokemon by ID

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, HTTPException, Request
+from sqlmodel import Session
 from starlette.responses import JSONResponse
 from db import create_all_tables, SessionDep
 
 from model import PokemonBase, PokemonID, PokemonUpdate
 from operation_csv import createPokemon, showPokemons, showPokemon, updatePokemon, deletePokemon
-from operation_db import create_pokemon_db
+from operation_db import create_pokemon_db, find_one_pokemon
 
 app = FastAPI(lifespan=create_all_tables)
 
@@ -20,7 +21,7 @@ async def http_exception_handler(request: Request, exc: HTTPException):
     return JSONResponse(status_code=exc.status_code, content={"message": f"{exc.detail} Ooops. TODOOO fallo"},)
 
 
-@app.post("/pokemon", response_model=PokemonID)
+@app.post("/pokemon", response_model=PokemonID, status_code=201)
 async def catch_pokemon(pokemon: PokemonBase, session:SessionDep):
      return create_pokemon_db(pokemon, session)
     ##pokedex.append(pokemon)
@@ -34,10 +35,9 @@ async def show_all_pokemon():
     return pokemons
 
 
-
 @app.get("/pokemon/{id}", response_model=PokemonID)
-async def show_pokemon(id: int):
-    pokemon = showPokemon(id)
+async def show_pokemon(id: int, session:SessionDep):
+    pokemon = find_one_pokemon(id, session)
     if not(pokemon):
         raise HTTPException(status_code=404, detail="Pokemon has not been caught")
     return pokemon
--- operation_db.py
+++ operation_db.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session
 from model import PokemonBase, PokemonID
 
@@ -7,5 +8,10 @@ def create_pokemon_db(pokemon:PokemonBase, session: Session):
     session.add(new)
     session.commit()
     session.refresh(new)
-
     return new
+
+def find_one_pokemon(pokemon_id:int, session: Session):
+    try:
+        return session.get_one(PokemonID, pokemon_id)
+    except NoResultFound:
+        return None
