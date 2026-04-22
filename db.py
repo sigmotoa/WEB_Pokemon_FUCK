ac06a8d75ff9b01a64e802cffd8c5667dbbfe28f
sigmotoa <si.galvismotoa@gmail.com>
2026-04-22 08:04:53 -0500

    [UPDATE] added other DB - postgresql

--- db.py
+++ db.py
@@ -1,11 +1,19 @@
+import os
+
 from sqlmodel import Session, create_engine, SQLModel
 from typing import Annotated
 from fastapi import Depends, FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+neon_url_db = os.getenv("DATABASE_URL")
+
 
 sqlite_name = "pokemondb.sqlite3"
 sqlite_url = f"sqlite:///{sqlite_name}"
 
-engine = create_engine(sqlite_url)
+engine = create_engine(neon_url_db)
 
 def create_all_tables(app: FastAPI):
     SQLModel.metadata.create_all(engine)
