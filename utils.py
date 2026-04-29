6cf4be52632099f05a74028d493929d91b1fb8a0
sigmotoa <si.galvismotoa@gmail.com>
2026-04-29 08:33:28 -0500

    [ADD] utility to sotre images in a supabase bucket

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
 import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
 from pathlib import Path
 import shutil
 from fastapi import UploadFile, File, HTTPException
 
-IMG_DIR = Path("files/img")
+load_dotenv()
 
-def save_img(file: UploadFile):
+
+IMG_DIR = Path("files/img")
+SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET")
+
+def save_img_local(file: UploadFile):
     if not file.content_type.startswith("image/"):
         raise HTTPException(status_code=400, detail="Invalid file type")
 
@@ -18,7 +24,33 @@ def save_img(file: UploadFile):
     return destination
 
 
+##SUPABASE Begins here
+def supabase_client():
+    url = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_KEY")
 
+    if not url or not key:
+        raise RuntimeError("No credentials provided")
+    return create_client(url, key)
+
+
+
+def save_img_remote(file: UploadFile):
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="Invalid file type")
+
+    contents = file.file.read()
+    path = file.filename
+
+    client = supabase_client()
+
+    response = client.storage.from_(SUPABASE_BUCKET).upload(
+        path=path,
+        file=contents,
+        file_options={"content-type":file.content_type},
+    )
+    public_url_bucket = client.storage.from_(SUPABASE_BUCKET).get_public_url(path)
+    return public_url_bucket
 
 
 
